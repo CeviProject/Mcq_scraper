48d8ed212f1097952697a43aa65d733bc7868df8
Cevi Project <ceviproject08@gmail.com>
2025-06-21 20:34:13 +0000

    Can u remove the google signin or github as its not there

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -93,7 +93,6 @@ export default function LoginForm() {
                             }
                         }
                     }}
-                    providers={['google', 'github']}
                 />
             </CardContent>
         </Card>
